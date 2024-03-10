494bec67755078cfcf380982a05e221b9af6ef62
Michel Kreutner <mkreutner@gmail.com>
2024-03-10 19:10:34 +0100

    tuples and output parameters

---
+++ code/Exercices/QuizzCapitales/OutilsMaths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace QuizzCapitales
+{
+	internal class OutilsMaths
+	{
+        public static (double, double) GetValeursMinMax(params double[] nombres)
+        {
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            foreach (double nombre in nombres)
+            {
+                if (nombre > max) max = nombre;
+                if (nombre < min) min = nombre;
+            }
+
+            return (min, max);
+        }
+
+        public static void GetValeursMinMax(out double min, out double max, params double[] nombres)
+        {
+            min = double.MaxValue;
+            max = double.MinValue;
+            foreach (double nombre in nombres)
+            {
+                if (nombre > max) max = nombre;
+                if (nombre < min) min = nombre;
+            }
+        }
+    }
+}
+
--- code/Exercices/QuizzCapitales/Program.cs
+++ code/Exercices/QuizzCapitales/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Quizz.Jouer(1,5,8,9,50);
+        //Quizz.Jouer(1,5,8,9,50);
+        OutilsMaths.GetValeursMinMax(out double mini, out double maxi, 4.21, 5.54, 2.13, 3.47);
+        Console.WriteLine($"Valeurs mini et maxi : {mini}, {maxi}");
+
+        double min, max;
+        OutilsMaths.GetValeursMinMax(out min, out max, 4.21, 5.54, 2.13, 3.47);
+
+        bool repOk;
+        do
+        {
+            Console.WriteLine("Saisissez un nombre compris entre 1 et 10 :");
+            string? rep = Console.ReadLine();
+            repOk = int.TryParse(rep, out int nombre) && nombre >= 1 && nombre <= 10;
+        } while (!repOk);
     }
 }
 
